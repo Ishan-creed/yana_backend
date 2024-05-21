8abd976e8af7a40adab350f4b7bb097816feae48
Ishan Verma <83359975+Ishan-creed@users.noreply.github.com>
2024-05-21 12:52:09 +0530

    Update connectDB.js

--- database/connectDB.js
+++ database/connectDB.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const Connection = () => {
 
-    mongoose.connect("mongodb+srv://myUser:Ua54u9U7Z4k70utN@cluster0.wk0dk.mongodb.net/love?retryWrites=true&w=majority", {
+    mongoose.connect("mongodb+srv://myUser:user12345@cluster0.wk0dk.mongodb.net/love?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
